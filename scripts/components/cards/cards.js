d25c003ec57037b8fe46d7592468f3cc3fd2ae87
Your Name <you@example.com>
2022-11-10 15:11:56 +0300

    upd: docs finalized

--- scripts/components/cards/cards.js
+++ scripts/components/cards/cards.js
@@ -6,17 +6,3 @@ document.querySelectorAll('.big-cards__item').forEach(item => {
         item.classList.toggle('shadow');
     })
 });
-
-// init image lists for cards 
-// function createImgEl(id, i) {
-//     return `<img class="small-card__before-after" src='../../images/cards/before-after/${id}/${id}_${i}.jpg'>`;
-// }
-// document.querySelectorAll('.small-cards__item').forEach((item, index) => {
-//     for(let i = 1; i < 11; i++) {
-//         const el = createImgEl(item.id, i);
-//         if (el === undefined) {
-//             continue;
-//         }
-//         item.insertAdjacentHTML('beforeend', el);
-//     }
-// })
